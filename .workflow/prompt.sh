0a0c86230590f32ca18049972fd8beb5804c679c
Nathan Miller <nmiller217@gmail.com>
2025-10-21 07:49:27 -0700

    feat(workflow/devcontainer): add LLM client selection to prompt.sh and run opencode on devcontainer start

--- .workflow/prompt.sh
+++ .workflow/prompt.sh
@@ -1,5 +1,32 @@
 #!/bin/sh
 
-prompt="/orchestrate-project-setup"
+# add if state,ment to branch on different LLM clients
+# based on $1 argument passed to the script: "claude" or "opencode"
+# otherwise show usage and exit
 
-claude --verbose --permission-mode bypassPermissions $prompt
+echo "[prompt.sh - v0.1.1] - invoke LLM client with prompt"
+echo "----------------------------------------------------"
+echo ""
+case "$(echo "$1" | tr '[:upper:]' '[:lower:]')" in
+  claude)
+    # claude code to orchestrate project setup
+    client="claude"
+    cliargs="--verbose --permission-mode bypassPermissions"
+    prompt="/orchestrate-project-setup"
+    ;;
+  opencode)
+    # opencode code to orchestrate project setup
+    client="opencode"
+    cliargs="--prompt"
+    prompt="/orchestrate-project-setup"
+    ;;
+  *)
+    echo "Usage: $0 {claude|opencode}"
+    exit 1
+    ;;
+esac
+
+echo "invoking: \"$client $cliargs $prompt\"..."
+
+# execute the orchestration command to the LLM client
+$client $cliargs $prompt
